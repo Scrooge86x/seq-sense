592311a1ff7fcef9b91aa0ac1d64423d1d6c353f
Scrooge86x <53798834+Scrooge86x@users.noreply.github.com>
2025-03-08 19:50:53 +0100

    Refactored settings.js.

--- game/js/settings.js
+++ game/js/settings.js
@@ -1,92 +1,94 @@
-// TODO: Just refactor this entangled shit
-
 import { questions } from './questions/questions.js';
 
-const settingsEl = document.querySelector('#settings');
-const getCurrentHeight = () => `${settingsEl.getBoundingClientRect().height}px`;
+export let activeLanguage = 'Polish';
+export let activeMode = 'monthsToNumbers';
 
 let languageChangeCallback = () => { };
 export const onLanguageChange = callback => {
     languageChangeCallback = callback;
 };
 
-const languageButtons = {};
-const modeButtons = {};
-
-export let activeLanguage = 'Polish';
-export let activeMode = 'monthsToNumbers';
-
-const languageSettingsEl = document.querySelector('#settings-languages');
-for (const language in questions) {
-    const button = document.createElement('button');
-    button.innerText = language;
-    button.addEventListener('click', () => setActiveLanguage(language));
-    languageSettingsEl.appendChild(button);
-    languageButtons[language] = button;
-}
-
-const setActiveLanguage = language => {
-    if (!(language in languageButtons))
-        return;
-
-    const oldButton = languageButtons[activeLanguage];
-    oldButton.removeAttribute('style');
-
-    const newButton = languageButtons[language];
-    newButton.style.borderColor = '#7fff00';
-
-    activeLanguage = language;
-    for (const button in modeButtons) {
-        modeButtons[button].innerText = questions[activeLanguage][button].name;
-    }
-    languageChangeCallback();
-};
-setActiveLanguage('Polish');
-
 let modeChangeCallback = () => { };
 export const onModeChange = callback => {
     modeChangeCallback = callback;
 };
 
-const modeSettingsEl = document.querySelector('#settings-modes');
-for (const mode in questions[activeLanguage]) {
-    const button = document.createElement('button');
-    button.innerText = questions[activeLanguage][mode].name;
-    button.addEventListener('click', () => setActiveMode(mode));
-    modeSettingsEl.appendChild(button);
-    modeButtons[mode] = button;
-}
+// Mode buttons must be global so setActiveLanguage can translate them.
+const g_modeButtons = {};
+const initModeButtons = () => {
+    const setActiveMode = mode => {
+        if (!(mode in g_modeButtons))
+            return;
 
-const setActiveMode = mode => {
-    if (!(mode in modeButtons))
-        return;
+        const oldButton = g_modeButtons[activeMode];
+        oldButton.removeAttribute('style');
 
-    const oldButton = modeButtons[activeMode];
-    oldButton.removeAttribute('style');
+        const newButton = g_modeButtons[mode];
+        newButton.style.borderColor = '#7fff00';
 
-    const newButton = modeButtons[mode];
-    newButton.style.borderColor = '#7fff00';
+        activeMode = mode;
+        modeChangeCallback();
+    };
 
-    activeMode = mode;
-    modeChangeCallback();
+    const modeSettingsEl = document.querySelector('#settings-modes');
+    for (const mode in questions[activeLanguage]) {
+        const button = document.createElement('button');
+        button.innerText = questions[activeLanguage][mode].name;
+        button.addEventListener('click', () => setActiveMode(mode));
+        modeSettingsEl.appendChild(button);
+        g_modeButtons[mode] = button;
+    }
+    setActiveMode(activeMode);
 };
-setActiveMode('monthsToNumbers');
+
+const initLanguageButtons = () => {
+    const languageButtons = {};
+    const setActiveLanguage = language => {
+        if (!(language in languageButtons))
+            return;
+
+        const oldButton = languageButtons[activeLanguage];
+        oldButton.removeAttribute('style');
+
+        activeLanguage = language;
+        const newButton = languageButtons[activeLanguage];
+        newButton.style.borderColor = '#7fff00';
+
+        for (const button in g_modeButtons) {
+            g_modeButtons[button].innerText = questions[activeLanguage][button].name;
+        }
+        languageChangeCallback();
+    };
+
+    const languageSettingsEl = document.querySelector('#settings-languages');
+    for (const language in questions) {
+        const button = document.createElement('button');
+        button.innerText = language;
+        button.addEventListener('click', () => setActiveLanguage(language));
+        languageSettingsEl.appendChild(button);
+        languageButtons[language] = button;
+    }
+    setActiveLanguage(activeLanguage);
+};
+
+initModeButtons();
+initLanguageButtons();
+
+const settingsEl = document.querySelector('#settings');
+const getCurrentHeightPx = () => `${settingsEl.getBoundingClientRect().height}px`;
+const initialHeight = getCurrentHeightPx();
 
 let visible = true;
-const fullHeight = getCurrentHeight();
-export const toggleSettings = async () => {
-    const startHeight = getCurrentHeight();
-    const endHeight = visible ? '0px' : fullHeight;
+export const toggleSettings = () => {
+    const startHeight = getCurrentHeightPx();
+    const endHeight = visible ? '0px' : initialHeight;
 
-    settingsEl.animate(
-        [
-            { maxHeight: startHeight },
-            { maxHeight: endHeight },
-        ],
-        {
-            duration: 200,
-        },
-    );
+    settingsEl.animate([
+        { maxHeight: startHeight },
+        { maxHeight: endHeight },
+    ], {
+        duration: 200,
+    });
 
     settingsEl.style.maxHeight = endHeight;
     visible = !visible;
